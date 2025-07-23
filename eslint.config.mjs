60a10b0ca87be5d78e87448741f3f982c4c14ab7
Rafał Berezin <rafalberezin@protonmail.com>
2025-07-23 14:44:04 +0200

    chore: update eslint and prettier configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,39 @@
 import prettier from 'eslint-config-prettier'
-import js from '@eslint/js'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 import { defineConfig } from 'eslint/config'
 
-export default defineConfig([
-	{ files: ['src/**/*.ts'], plugins: { js }, extends: ['js/recommended'] },
-	{ files: ['src/**/*.ts'], languageOptions: { globals: globals.browser } },
-	tseslint.configs.recommended,
-	prettier,
-	{
-		rules: {
-			'@typescript-eslint/no-unused-vars': [
-				'error',
-				{
-					argsIgnorePattern: '^_',
-					varsIgnorePattern: '^_'
+function scoped(scope, configs) {
+	return configs.map(config => ({ ...config, files: scope }))
+}
+
+export default defineConfig(
+	scoped(
+		['src/**/*.ts'],
+		[
+			...tseslint.configs.recommended,
+			prettier,
+
+			{
+				languageOptions: {
+					parserOptions: {
+						sourceType: 'module'
+					},
+					globals: {
+						...globals.browser,
+						...globals.node
+					}
+				},
+				rules: {
+					'@typescript-eslint/no-unused-vars': [
+						'error',
+						{
+							argsIgnorePattern: '^_',
+							varsIgnorePattern: '^_'
+						}
+					]
 				}
-			]
-		}
-	}
-])
+			}
+		]
+	)
+)
