20321f7d515b6c659058d3be0b996a768ff3dd48
Rafał Berezin <rafalberezin@protonmail.com>
2025-08-05 00:23:25 +0200

    feat(installer): add automatic dev tools hook injection

--- src/modloader-installer/main.go
+++ src/modloader-installer/main.go
@@ -3,59 +3,87 @@ package main
 import (
 	"bytes"
 	_ "embed"
+	"flag"
 	"fmt"
 	"os"
+	"regexp"
 	"slices"
 )
 
 const (
-	indexPath     = "win-unpacked/resources/app/game/index.html"
+	indexHtmlFile = "win-unpacked/resources/app/game/index.html"
+	mainJsFile    = "win-unpacked/resources/app/main.js"
 	modloaderDir  = "modloader/"
-	modloaderPath = modloaderDir + "modloader.js"
-	modsPath      = "mods/"
+	modloaderFile = modloaderDir + "modloader.js"
+	modsDir       = "mods/"
+
+	flagDevTools = "--dev"
 )
 
 var (
-	importScript = []byte("<script src=\"../../../../modloader/modloader.js\"></script>\n")
-	injectBefore = []byte("</head>")
+	injectModloaderScript = []byte("\t<script src=\"../../../../modloader/modloader.js\"></script>\n")
+	injectModloaderRegex  = regexp.MustCompile("<script src=\"../../../../modloader/modloader.js\">\\s*</script>")
+	injectModloaderBefore = []byte("</head>")
+
+	injectDevToolsScript = []byte("\nipcMain.on('ml-dt', () => win.webContents.openDevTools());\n")
+	injectDevToolsRegex  = regexp.MustCompile("ipcMain\\.on\\(['\"`]ml-dt['\"`], ?\\(\\) ?=> ?win\\.webContents\\.openDevTools\\(\\)\\);?")
 )
 
-// Chill out go-staticcheck, you'll get the file at compile time.
+// Chill out go, you'll get the file at compile time.
 
 //go:embed modloader.js
 var modloader []byte
 
+type Flags struct {
+	dev    bool
+	nowait bool
+}
+
+func parseFlags() *Flags {
+	flags := Flags{}
+	flag.BoolVar(&flags.dev, "dev", false, "Install the DevTools opening hook")
+	flag.BoolVar(&flags.nowait, "nowait", false, "Don't wait for user input after installation")
+
+	flag.Parse()
+
+	return &flags
+}
+
 func main() {
+	flags := parseFlags()
+
 	fmt.Println("[ INFO ] Installing modloader.")
 
-	defer func() {
-		fmt.Print("Press [ENTER] to exit.")
-		fmt.Scanln()
-	}()
+	if !flags.nowait {
+		defer func() {
+			fmt.Println("\nPress [ENTER] to exit.")
+			fmt.Scanln()
+		}()
+	}
 
-	fmt.Printf("[ INFO ] Injecting modloader script into %q.\n", indexPath)
+	fmt.Printf("[ INFO ] Injecting modloader script into %q.\n", indexHtmlFile)
 
-	data, err := os.ReadFile(indexPath)
+	data, err := os.ReadFile(indexHtmlFile)
 	if err != nil {
 		fmt.Printf(
-			"[ ERROR ] Failed to open %q for modloader injection: %s\nDid you run the installer inside the \"Sixty Four\" game directory (next to \"win-unpacked\")?\n\n",
-			indexPath, err)
+			"[ ERROR ] Failed to open %q for modloader injection: %s\nDid you run the installer inside the \"Sixty Four\" game directory (next to \"win-unpacked\")?\n",
+			indexHtmlFile, err)
 		return
 	}
 
-	if bytes.Contains(data, importScript) {
-		fmt.Printf("[ INFO ] Skipping step: modloader script already injected.\n")
+	if injectModloaderRegex.Match(data) {
+		fmt.Println("[ INFO ] Skipping step: modloader script already injected.")
 	} else {
-		i := bytes.Index(data, injectBefore)
+		i := bytes.Index(data, injectModloaderBefore)
 		if i < 0 {
-			fmt.Printf("[ ERROR ] Failed to inject modloader into %q.\n\n", indexPath)
+			fmt.Printf("[ ERROR ] Failed to inject modloader into %q.\n", indexHtmlFile)
 			return
 		}
 
-		data = slices.Insert(data, i, importScript...)
+		data = slices.Insert(data, i, injectModloaderScript...)
 
-		if err := os.WriteFile(indexPath, data, 0666); err != nil {
-			fmt.Printf("[ ERROR ] Failed to inject modloader into %q.\n\n", indexPath)
+		if err := os.WriteFile(indexHtmlFile, data, 0644); err != nil {
+			fmt.Printf("[ ERROR ] Failed to inject modloader into %q.\n", indexHtmlFile)
 			return
 		}
 	}
@@ -64,19 +92,46 @@ func main() {
 
 	err = createDir(modloaderDir)
 	if err == nil {
-		err = os.WriteFile(modloaderPath, modloader, 0666)
+		err = os.WriteFile(modloaderFile, modloader, 0644)
 	}
 
 	if err != nil {
-		fmt.Printf("[ ERROR ] Failed to unpack modloader files: %s.\n\n", err)
+		fmt.Printf("[ ERROR ] Failed to unpack modloader files: %s.\n", err)
 		return
 	}
 
-	if err := createDir(modsPath); err != nil {
-		fmt.Printf("[ WARNING ] Failed to create the %q directory: %s.\n", modsPath, err)
+	if err := createDir(modsDir); err != nil {
+		fmt.Printf("[ WARNING ] Failed to create the %q directory: %s.\n", modsDir, err)
 	}
 
-	fmt.Printf("[ SUCCESS ] Successfully installed the modloader. Place your mod files (.js) into the %q directory.\n\n", modsPath)
+	defer fmt.Printf("[ SUCCESS ] Successfully installed the modloader. Place your mod files (.js) into the %q directory.\n\n", modsDir)
+
+	if !flags.dev {
+		return
+	}
+
+	fmt.Printf("[ INFO ] %q flag present: Injecting dev tools hook into %q.\n", flagDevTools, mainJsFile)
+
+	data, err = os.ReadFile(mainJsFile)
+	if err != nil {
+		fmt.Printf(
+			"[ ERROR ] Failed to open %q for dev tools hook injection: %q.\n", mainJsFile, err)
+		return
+	}
+
+	if injectDevToolsRegex.Match(data) {
+		fmt.Printf("[ INFO ] Skipping step: dev tools hook already injected.\n")
+		return
+	}
+
+	data = append(data, injectDevToolsScript...)
+
+	if err := os.WriteFile(mainJsFile, data, 0644); err != nil {
+		fmt.Printf("[ ERROR ] Failed to inject dev tools hook %q.\n", mainJsFile)
+		return
+	}
+
+	fmt.Printf("[ SUCCESS ] Successfully injected the dev tools hook. Press \"Ctrl + Shift + I\" in game to open dev tools.\n")
 }
 
 func createDir(name string) error {
